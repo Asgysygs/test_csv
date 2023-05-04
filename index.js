37e74d5877c2b54ca0774d8345a884fa502fbd9f
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-05-04 14:53:24 +0300

    Step 1 done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash';
 
-export default function solution(content){
+export default function solution(content) {
   // BEGIN
-  console.log(content);
+
+  // Data normalization
+  const data = content.split('\r\n');
+  const passengers = data.slice(1, data.length - 1);
+  const normalizedData = passengers.map((passenger) => passenger.split(','));
+
+  // Step 1
+  const passengersCount = normalizedData[normalizedData.length - 1];
+  console.log(`Количество пассажиров: ${passengersCount[0]}`);
+
   // END
 }
